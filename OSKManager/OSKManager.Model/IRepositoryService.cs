ff2cf79d13880366bb5bf3b86a0b5ff0acfefd63
Józef Salik <jozek243@gmail.com>
2020-04-26 19:06:28 +0200

    Remove unuses references.

--- OSKManager/OSKManager.Model/IRepositoryService.cs
+++ OSKManager/OSKManager.Model/IRepositoryService.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.Exchange.WebServices.Data;
-using OSKManager.Model;
+﻿using OSKManager.Model;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 
--- OSKManager/OSKManager.Model/RepositoryService.cs
+++ OSKManager/OSKManager.Model/RepositoryService.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Exchange.WebServices.Data;
 using OSKManager.Model;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 
