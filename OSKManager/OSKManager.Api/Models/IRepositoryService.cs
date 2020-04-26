bb0ae98458cea9bf3b3a0e52e86115ed23616d83
Tomasz Zając <zajact19@gmail.com>
2020-04-26 18:02:27 +0200

    Add IRepositoryService and RepositoryService

---
+++ OSKManager/OSKManager.Api/Models/IRepositoryService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Exchange.WebServices.Data;
+using OSKManager.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace OSKManager.Api.Models
+{
+    public interface IRepositoryService<T> where T : IEntity<int>
+    {
+
+        Task<T> GetSingle(int id);
+        Task<IEnumerable<T>> FindBy(Expression<Func<T, bool>> predicate);
+        Task<T> Add(T entity);
+        Task<T> Delete(T entity);
+        Task<T> Edit(T entity);
+    }
+}
