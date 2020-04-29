a47ef76a545f5042dc9adc188a4c52812cddc445
Tomasz Zając <zajact19@gmail.com>
2020-04-29 22:28:42 +0200

    Adding CoursesController

---
+++ OSKManager/OSKManager.Api/Controllers/CoursesController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OSKManager.Api.Models;
+using OSKManager.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OSKManager.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesController : ControllerBase
+    {
+        private readonly IRepositoryService<Course> repositoryService;
+
+        public CoursesController(IRepositoryService<Course> repositoryService)
+        {
+            this.repositoryService = repositoryService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetCourses()
+        {
+            try
+            {
+                return Ok(repositoryService.GetAllRecords());
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error retrieving data from the database");
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Course>> GetCourses(Guid id)
+        {
+            try
+            {
+                var result = await repositoryService.GetSingle(id);
+
+                if (result == null)
+                {
+                    return NotFound();
+                }
+
+                return result;
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error retrieving data from the database");
+            }
+        }
+    }
+}
