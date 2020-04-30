9210dbd7cd125a77a3a84735168c9662201c9cf7
Józef Salik <jozek243@gmail.com>
2020-04-30 11:21:25 +0200

    Class ApplicationDbContext now inherits from IdentityDbContext (not DbContext).

--- OSKManager/OSKManager.Model/ApplicationDbContext.cs
+++ OSKManager/OSKManager.Model/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using OSKManager.Model.Configuration;
 
 namespace OSKManager.Model
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions <ApplicationDbContext> options) 
             : base (options)
@@ -12,6 +13,7 @@ namespace OSKManager.Model
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfiguration(new CategoryConfiguration());
         }
 
