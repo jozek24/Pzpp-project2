22a08ef68a29a3b482b596820555b963835776cf
Tomasz Zając <zajact19@gmail.com>
2020-05-26 12:04:14 +0200

    Adding ShowFooter for administrator

--- OSKManager/OSKManager.Web/Areas/Administrator/Pages/CalendarBase.cs
+++ OSKManager/OSKManager.Web/Areas/Administrator/Pages/CalendarBase.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace OSKManager.Web.Areas.Administrator.Pages
 {
-    public class CalendarBase
+    public class CalendarBase : ComponentBase
     {
+        public bool ShowFooter { get; set; } = true;
+
+        public int Days { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Days = DateTime.DaysInMonth(2020, 5);
+        }
     }
 }
--- OSKManager/OSKManager.Web/Areas/Administrator/Pages/DisplayDayBase.cs
+++ OSKManager/OSKManager.Web/Areas/Administrator/Pages/DisplayDayBase.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace OSKManager.Web.Areas.Administrator.Pages
 {
-    public class DisplayDayBase
+    public class DisplayDayBase : ComponentBase
     {
+        [Parameter]
+        public bool ShowFooter { get; set; }
     }
 }
