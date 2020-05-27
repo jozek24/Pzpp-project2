56fc666afa24a0046b2edee36e0f2a8f79ec98aa
KonGarli <garlaczkonrad@gmail.com>
2020-05-27 17:59:01 +0200

    Add CurseSingUp Page, update ClientPanel

---
+++ OSKManager/OSKManager.Web/Pages/ClientPages/SingUpCursBase.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using OSKManager.Model;
+using OSKManager.Web.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace OSKManager.Web.Pages.ClientPages
+{
+    public class SingUpCursBase : ComponentBase
+    {
+        [Inject]
+        public  ICourseService CoursesService { get; set; }
+        public List<Course> Courses { get; set; } = new List<Course>();
+        protected async override Task OnInitializedAsync()
+        {
+            Courses = (await CoursesService.GetCourses()).ToList();
+
+           
+        }
+    }
+}
