0a5146ca051ffa0d85e83f4465847f5999945d66
lukno <lukasz1081@wp.pl>
2020-06-01 23:17:10 +0200

    add moq and test to Delete Course

--- OSKManager/OSKManager.Api.Tests/Controllers/CoursesControllerTests.cs
+++ OSKManager/OSKManager.Api.Tests/Controllers/CoursesControllerTests.cs
@@ -1,14 +1,18 @@
-﻿using Moq;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
 using OSKManager.Api.Controllers;
 using OSKManager.Api.Models;
 using OSKManager.Model;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace OSKManager.Api.Tests.Controllers
 {
-    
+
     public class CoursesControllerTests
     {
         private readonly Mock<IRepositoryService<Course>> _mockRepo;
@@ -17,6 +21,28 @@ namespace OSKManager.Api.Tests.Controllers
         {
             _mockRepo = new Mock<IRepositoryService<Course>>();
             _controller = new CoursesController(_mockRepo.Object);
+
+
+        }
+
+        [Fact]
+        public void DeleteCourse_WhenCalled_DeleteCourse()
+        {
+            var course = new Course
+            {
+                Id = Guid.NewGuid()
+            };
+            Course course2 = null;
+
+            _mockRepo.Setup(r => r.GetSingle(course.Id))
+                .Returns(Task.FromResult(course));
+
+            var actionResult = _controller.DeleteCourse(course.Id);
+            var result = actionResult.Result.Result as ObjectResult;
+            Assert.NotNull(result);
+            Assert.IsType<NotFoundResult>(result);
+            Assert.Equal(StatusCodes.Status404NotFound, result.StatusCode);
+
 
 
         }
