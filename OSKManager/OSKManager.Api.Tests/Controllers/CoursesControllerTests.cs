b92aae39a2e09e8f3bf2f3b986a9c273dd49b63e
lukno <lukasz1081@wp.pl>
2020-06-01 13:46:36 +0200

    Add class CoursesControllerTests into tests

---
+++ OSKManager/OSKManager.Api.Tests/Controllers/CoursesControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using OSKManager.Api.Controllers;
+using OSKManager.Api.Models;
+using OSKManager.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OSKManager.Api.Tests.Controllers
+{
+    
+    public class CoursesControllerTests
+    {
+        private readonly Mock<IRepositoryService<Course>> _mockRepo;
+        private readonly CoursesController _controller;
+        public CoursesControllerTests()
+        {
+            _mockRepo = new Mock<IRepositoryService<Course>>();
+            _controller = new CoursesController(_mockRepo.Object);
+
+
+        }
+    }
+}
