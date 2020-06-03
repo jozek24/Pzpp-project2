8e8c24bfe55799172caa03945025ef17ec860a8a
KonGarli <garlaczkonrad@gmail.com>
2020-06-03 16:11:55 +0200

    Add CategoryController and IServices

---
+++ OSKManager/OSKManager.Web/Services/CategoryService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Components;
+using OSKManager.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
+namespace OSKManager.Web.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly HttpClient httpClient;
+
+        public CategoryService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<Category> CreateCategory(Category newCategory)
+        {
+            return await httpClient.PostJsonAsync<Category>("api/categories", newCategory);
+        }
+
+        public async Task DeleteCategory(Guid id)
+        {
+            await httpClient.DeleteAsync($"api/categories/{id}");
+        }
+
+        public async Task<IEnumerable<Category>> GetCategories()
+        {
+            return await httpClient.GetJsonAsync<Category[]>($"api/categories");
+        }
+
+        public async Task<Category> GetCategory(Guid id)
+        {
+            return await httpClient.GetJsonAsync<Category>($"api/categories/{id}");
+        }
+
+        public async Task<Category> UpdateCategory(Category updatedCategory)
+        {
+            return await httpClient.PutJsonAsync<Category>("api/categories", updatedCategory);
+        }
+    }
+}
---
+++ OSKManager/OSKManager.Web/Services/ICategoryService.cs
@@ -0,0 +1,17 @@
+﻿using OSKManager.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OSKManager.Web.Services
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<Category>> GetCategories();
+        Task<Category> GetCategory(Guid id);
+        Task<Category> UpdateCategory(Category updatedCategory);
+        Task<Category> CreateCategory(Category newCategory);
+        Task DeleteCategory(Guid id);
+    }
+}
