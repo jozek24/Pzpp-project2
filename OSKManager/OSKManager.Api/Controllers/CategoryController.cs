85c90b1c061a19c8cefb0e50106f78c2e2b9f708
KonGarli <garlaczkonrad@gmail.com>
2020-06-03 19:12:02 +0200

    Add Controler Using

--- OSKManager/OSKManager.Api/Controllers/CategoryController.cs
+++ OSKManager/OSKManager.Api/Controllers/CategoryController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OSKManager.Api.Models;
@@ -21,6 +22,7 @@ namespace OSKManager.Api.Controllers
         }
 
         [HttpGet]
+        
         public async Task<ActionResult> GetCategories()
         {
             try
