44123b64b1bce4955eceab483e8f633cda89a902
Tomasz Zając <zajact19@gmail.com>
2020-06-15 10:29:02 +0200

    Changes

--- OSKManager/OSKManager.Api/Controllers/InstructorsController.cs
+++ OSKManager/OSKManager.Api/Controllers/InstructorsController.cs
@@ -1,34 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OSKManager.Api.Models;
 using OSKManager.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace OSKManager.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize(Roles = "Student,Instructor,Administrator")]
-    public class CategoryController : ControllerBase
+    [Authorize(Roles = "Instructor")]
+    public class InstructorsController : ControllerBase
     {
-        private readonly IRepositoryService<Category> repositoryService;
+        private readonly IRepositoryService<Instructor> repositoryService;
 
-        public CategoryController(IRepositoryService<Category> repositoryService)
+        public InstructorsController(IRepositoryService<Instructor> repositoryService)
         {
             this.repositoryService = repositoryService;
         }
 
         [HttpGet]
-        public async Task<ActionResult> GetCategories()
+        public async Task<ActionResult> GetInstructors()
         {
             try
             {
                 return Ok(await repositoryService.GetAllRecords());
-             
+
             }
             catch (Exception)
             {
@@ -38,7 +38,7 @@ namespace OSKManager.Api.Controllers
         }
 
         [HttpGet("{id:Guid}")]
-        public async Task<ActionResult<Category>> GetCategory(Guid id)
+        public async Task<ActionResult<Instructor>> GetInstructor(Guid id)
         {
             try
             {
@@ -59,19 +59,19 @@ namespace OSKManager.Api.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<Course>> CreateCategory(Category category)
+        public async Task<ActionResult<Instructor>> CreateInstructor(Instructor instructor)
         {
             try
             {
-                if (category == null)
+                if (instructor == null)
                 {
                     return BadRequest();
                 }
 
-                var createdCategory = await repositoryService.Add(category);
+                var createdInstructor = await repositoryService.Add(instructor);
 
-                return CreatedAtAction(nameof(GetCategory), new { id = createdCategory.Id },
-                    createdCategory);
+                return CreatedAtAction(nameof(GetInstructor), new { id = createdInstructor.Id },
+                    createdInstructor);
             }
             catch (Exception)
             {
@@ -81,18 +81,18 @@ namespace OSKManager.Api.Controllers
         }
 
         [HttpPut()]
-        public async Task<ActionResult<Category>> UpdateCategory(Category category)
+        public async Task<ActionResult<Instructor>> UpdateInstructor(Instructor instructor)
         {
             try
             {
-                var categoryToUpdate = await repositoryService.GetSingle(category.Id);
+                var instructorToUpdate = await repositoryService.GetSingle(Guid.Parse(instructor.Id));
 
-                if (categoryToUpdate == null)
+                if (instructorToUpdate == null)
                 {
-                    return NotFound($"Course with Id = {category.Id} not found");
+                    return NotFound($"Instructor with Id = {instructor.Id} not found");
                 }
 
-                return await repositoryService.Update(category);
+                return await repositoryService.Update(instructor);
             }
             catch (Exception)
             {
@@ -102,18 +102,18 @@ namespace OSKManager.Api.Controllers
         }
 
         [HttpDelete("{id:Guid}")]
-        public async Task<ActionResult<Category>> DeleteCategory(Guid id)
+        public async Task<ActionResult<Instructor>> DeleteInstructor(Guid id)
         {
             try
             {
-                var categoryToDelete = await repositoryService.GetSingle(id);
+                var instructorToDelete = await repositoryService.GetSingle(id);
 
-                if (categoryToDelete == null)
+                if (instructorToDelete == null)
                 {
-                    return NotFound($"Course with Id = {id} not found");
+                    return NotFound($"Instructor with Id = {id} not found");
                 }
 
-                return await repositoryService.Delete(categoryToDelete);
+                return await repositoryService.Delete(instructorToDelete);
             }
             catch (Exception)
             {
