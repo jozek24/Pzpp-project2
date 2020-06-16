2e511302526068e14ca482dffcdd9eebd36ac617
lukno <lukasz1081@wp.pl>
2020-06-16 17:18:54 +0200

    add js code "anchorLink" in anchorLink.js

--- OSKManager/OSKManager.Web/wwwroot/js/anchorLink.js
+++ OSKManager/OSKManager.Web/wwwroot/js/anchorLink.js
@@ -1,1 +1,9 @@
-﻿
+﻿window.anchorLink = {
+    scrollIntoView: function (elementId) {
+        var elem = document.getElementById(elementId);
+        if (elem) {
+            elem.scrollIntoView();
+            window.location.hash = elementId;
+        }
+    }
+}
