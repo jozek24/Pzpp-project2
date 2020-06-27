e79147041c96fe873f05541b10cdccf7a8f8ad58
Tomasz Zając <zajact19@gmail.com>
2020-06-27 17:55:39 +0200

    Adding DrivingDateService(HttpClient httpClient) constructor for DrivingDateService

--- OSKManager/OSKManager.Web/Services/DrivingDateService.cs
+++ OSKManager/OSKManager.Web/Services/DrivingDateService.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace OSKManager.Web.Services
 {
     public class DrivingDateService
     {
+        private readonly HttpClient httpClient;
+
+        public DrivingDateService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
     }
 }
