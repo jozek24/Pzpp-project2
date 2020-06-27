2b418f39b3d6df9a419f1227efd93c310b9c7038
Tomasz Zając <zajact19@gmail.com>
2020-06-27 17:28:41 +0200

    Adding DrivingDatesController with authorize for Instructors

---
+++ OSKManager/OSKManager.Api/Controllers/DrivingDatesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OSKManager.Api.Models;
+using OSKManager.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OSKManager.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Instructor")]
+    public class DrivingDatesController : ControllerBase
+    {
+        private readonly IRepositoryService<DrivingDate> repositoryService;
+
+        public DrivingDatesController(IRepositoryService<DrivingDate> repositoryService)
+        {
+            this.repositoryService = repositoryService;
+        }
+    }
+}
