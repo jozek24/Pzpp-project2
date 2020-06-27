749305efb14d5e9958acc8312c85ab6191fd663e
Tomasz Zając <zajact19@gmail.com>
2020-06-27 18:00:44 +0200

    Implement interface IDrivingDateService for DrivingDateService class

--- OSKManager/OSKManager.Web/Services/DrivingDateService.cs
+++ OSKManager/OSKManager.Web/Services/DrivingDateService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OSKManager.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -6,13 +7,38 @@ using System.Threading.Tasks;
 
 namespace OSKManager.Web.Services
 {
-    public class DrivingDateService
+    public class DrivingDateService : IDrivingDateService
     {
         private readonly HttpClient httpClient;
 
         public DrivingDateService(HttpClient httpClient)
         {
             this.httpClient = httpClient;
+        }
+
+        public Task<DrivingDate> CreateDrivingDate(DrivingDate newDrivingDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteDrivingDate(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DrivingDate> GetDrivingDate(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<DrivingDate>> GetDrivingDates()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DrivingDate> UpdatDrivingDate(DrivingDate updatedDrivingDate)
+        {
+            throw new NotImplementedException();
         }
     }
 }
