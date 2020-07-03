e49f4badd070c1c7641c4b49b26bdda27980b615
Tomasz Zając <zajact19@gmail.com>
2020-07-03 15:57:45 +0200

    Adding Categories property in TestData

--- OSKManager/OSKManager.Web.Tests/TestData.cs
+++ OSKManager/OSKManager.Web.Tests/TestData.cs
@@ -30,5 +30,52 @@ namespace OSKManager.Web.Tests
                 .AsQueryable();
             }
         }
+
+        public static IQueryable<Category> Categories
+        {
+            get
+            {
+                return new List<Category>
+                {
+                    new Category
+                    {
+                        Id = Guid.Parse("AC761785-ED42-11CE-DACB-00BDD0057645"),
+                        ShortCategory = "A",
+                        LongCategoryName = "Motocykl",
+                        BasicPrice = 1500,
+                        CountOfLectureHours = 30,
+                        CountOfDrivingHours = 20
+                    },
+                    new Category
+                    {
+                        Id = Guid.Parse("BC761785-ED42-11CE-DACB-00BDD0057645"),
+                        ShortCategory = "B",
+                        LongCategoryName = "Samochód Osobowy",
+                        BasicPrice = 1700,
+                        CountOfLectureHours = 30,
+                        CountOfDrivingHours = 30
+                    },
+                    new Category
+                    {
+                        Id = Guid.Parse("CC761785-ED42-11CE-DACB-00BDD0057645"),
+                        ShortCategory = "C",
+                        LongCategoryName = "Samochód Ciężarowy",
+                        BasicPrice = 2600,
+                        CountOfLectureHours = 20,
+                        CountOfDrivingHours = 30
+                    },
+                    new Category
+                    {
+                        Id = Guid.Parse("DC761785-ED42-11CE-DACB-00BDD0057645"),
+                        ShortCategory = "D",
+                        LongCategoryName = "Autobus",
+                        BasicPrice = 4800,
+                        CountOfLectureHours = 20,
+                        CountOfDrivingHours = 60
+                    }
+                }
+                .AsQueryable();
+            }
+        }
     }
 }
