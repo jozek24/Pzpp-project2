cea847a653fd45d399de17629b8b18f75fff6fae
Tomasz Zając <zajact19@gmail.com>
2020-07-03 11:48:13 +0200

    Adding constructor for TestAsyncEnumerator

--- OSKManager/OSKManager.Web.Tests/Helper/TestAsyncEnumerator.cs
+++ OSKManager/OSKManager.Web.Tests/Helper/TestAsyncEnumerator.cs
@@ -7,6 +7,13 @@ namespace OSKManager.Web.Tests.Helper
 {
     internal class TestAsyncEnumerator<T> : IAsyncEnumerator<T>
     {
+        private readonly IEnumerator<T> inner;
+
+        public TestAsyncEnumerator(IEnumerator<T> inner)
+        {
+            this.inner = inner;
+        }
+
         public T Current => throw new NotImplementedException();
 
         public ValueTask DisposeAsync()
