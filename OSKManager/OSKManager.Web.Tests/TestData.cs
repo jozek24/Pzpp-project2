da2d6c23bd4d8799989080b1b82e8182353759c3
Tomasz Zając <zajact19@gmail.com>
2020-07-03 15:36:34 +0200

    Adding DrivingDates property in TestData

--- OSKManager/OSKManager.Web.Tests/TestData.cs
+++ OSKManager/OSKManager.Web.Tests/TestData.cs
@@ -1,10 +1,34 @@
-﻿using System;
+﻿using OSKManager.Model;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace OSKManager.Web.Tests
 {
     public static class TestData
     {
+        public static IQueryable<DrivingDate> DrivingDates
+        {
+            get
+            {
+                return new List<DrivingDate>
+                {
+                    new DrivingDate 
+                    { 
+                        Id = Guid.Parse("AC761785-ED42-11CE-DACB-00BDD0057645"), 
+                        CourseId = Guid.Parse("BC761785-ED42-11CE-DACB-00BDD0057645"), 
+                        DateTime = DateTime.Now 
+                    },
+                    new DrivingDate 
+                    { 
+                        Id = Guid.Parse("CC761785-ED42-11CE-DACB-00BDD0057645"), 
+                        CourseId = Guid.Parse("DC761785-ED42-11CE-DACB-00BDD0057645"), 
+                        DateTime = DateTime.Now 
+                    }
+                }
+                .AsQueryable();
+            }
+        }
     }
 }
