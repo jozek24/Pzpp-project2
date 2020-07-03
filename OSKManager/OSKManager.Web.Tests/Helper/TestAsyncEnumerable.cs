6c4f425d38b82157f67e7125197f21a28623d9dc
Tomasz Zając <zajact19@gmail.com>
2020-07-03 11:37:40 +0200

    Adding constructor for TestAsyncEnumerable

--- OSKManager/OSKManager.Web.Tests/Helper/TestAsyncEnumerable.cs
+++ OSKManager/OSKManager.Web.Tests/Helper/TestAsyncEnumerable.cs
@@ -8,6 +8,10 @@ namespace OSKManager.Web.Tests.Helper
 {
     internal class TestAsyncEnumerable<T> : EnumerableQuery<T>, IAsyncEnumerable<T>, IQueryable<T>
     {
+        public TestAsyncEnumerable(IEnumerable<T> enumerable)
+            : base(enumerable)
+        { }
+
         public IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
