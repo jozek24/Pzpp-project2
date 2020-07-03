82d199cf5e0e4f49bc4e17789c805cde9d574876
Tomasz Zając <zajact19@gmail.com>
2020-07-03 11:35:48 +0200

    Implement EnumerableQuery<T>, IAsyncEnumerable<T>, IQueryable<T>for TestAsyncEnumerable internal class

--- OSKManager/OSKManager.Web.Tests/Helper/TestAsyncEnumerable.cs
+++ OSKManager/OSKManager.Web.Tests/Helper/TestAsyncEnumerable.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace OSKManager.Web.Tests.Helper
 {
-    internal class TestAsyncEnumerable<T>
+    internal class TestAsyncEnumerable<T> : EnumerableQuery<T>, IAsyncEnumerable<T>, IQueryable<T>
     {
+        public IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
