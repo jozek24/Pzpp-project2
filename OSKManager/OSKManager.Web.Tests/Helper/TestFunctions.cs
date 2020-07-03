86245882135d19f025e75da561889813a5eedc7a
Tomasz Zając <zajact19@gmail.com>
2020-07-03 11:19:06 +0200

    Adding GetDbSet method in TestFunctions class

--- OSKManager/OSKManager.Web.Tests/Helper/TestFunctions.cs
+++ OSKManager/OSKManager.Web.Tests/Helper/TestFunctions.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace OSKManager.Web.Tests.Helper
 {
     public static class TestFunctions
     {
+        public static Mock<DbSet<T>> GetDbSet<T>(IQueryable<T> TestData) where T : class
+        {
+            var MockSet = new Mock<DbSet<T>>();
+            MockSet.As<IAsyncEnumerable<T>>().Setup(x => x.GetEnumerator()).Returns(new TestAsyncEnumerator<T>(TestData.GetEnumerator()));
+            MockSet.As<IQueryable<T>>().Setup(x => x.Provider).Returns(new TestAsyncQueryProvider<T>(TestData.Provider));
+            MockSet.As<IQueryable<T>>().Setup(x => x.Expression).Returns(TestData.Expression);
+            MockSet.As<IQueryable<T>>().Setup(x => x.ElementType).Returns(TestData.ElementType);
+            MockSet.As<IQueryable<T>>().Setup(x => x.GetEnumerator()).Returns(TestData.GetEnumerator());
+            return MockSet;
+        }
     }
 }
