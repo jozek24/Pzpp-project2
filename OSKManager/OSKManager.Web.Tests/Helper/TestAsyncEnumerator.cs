834869ce8b12a1e1cab5e1d9c0a974ed17b226d5
Tomasz Zając <zajact19@gmail.com>
2020-07-03 11:46:17 +0200

    Implement IAsyncEnumerator<T> interface for TestAsyncEnumerator class

--- OSKManager/OSKManager.Web.Tests/Helper/TestAsyncEnumerator.cs
+++ OSKManager/OSKManager.Web.Tests/Helper/TestAsyncEnumerator.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace OSKManager.Web.Tests.Helper
 {
-    internal class TestAsyncEnumerator<T>
+    internal class TestAsyncEnumerator<T> : IAsyncEnumerator<T>
     {
+        public T Current => throw new NotImplementedException();
+
+        public ValueTask DisposeAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<bool> MoveNextAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
